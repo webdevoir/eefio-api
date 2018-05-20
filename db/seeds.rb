95c95305be1c9c3dacf0fe94171793700cc98e73
Shane Becker <veganstraightedge@gmail.com>
2018-05-20 12:55:12 -0700

    Add beginning of a seed script
    This will connect to an Ethereum node and get the latest block. Not very exciting yet.
    
    It uses a handful of default values… which uses Infura.io. If you use Infura, you’ll need to include
    your Infura API key in your `.env` at the root of this Rails application. It will not get committed or
    pushed, because it is ignored in `.gitignore`.
    
    If you run this in a `production` environment, like on Heroku, you’ll need to set the actual
    ENV variable. Your `.env` file will not be present or read in `production`.
    
    If you have your own Ethereum node and want to use it or change any the default values,
    just add those to your `.env` file and/or in your ENV variables in `production`.

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+# Setup in your .env file at the root of this Rails apps
+ETHEREUM_NODE_HOST         = ENV['ETHEREUM_NODE_HOST']          || 'mainnet.infura.io'.freeze
+ETHEREUM_NODE_PORT         = ENV['ETHEREUM_NODE_PORT']          || 443
+ETHEREUM_NODE_OPEN_TIMEOUT = ENV['ETHEREUM_NODE_OPEN_TIMEOUT']  || 20
+ETHEREUM_NODE_READ_TIMEOUT = ENV['ETHEREUM_NODE_READ_TIMEOUT']  || 140
+ETHEREUM_NODE_USE_SSL      = ENV['ETHEREUM_NODE_USE_SSL']       || true
+ETHEREUM_NODE_RPC_PATH     = ENV['ETHEREUM_NODE_RPC_PATH']      || '/'.freeze
+
+# README:
+# If you’re using Infura.io for your host, you’ll need to get an API key from their website.
+# Your Infura API key then needs to go into your .env file with a leading slash. For example:
+#     ETHEREUM_NODE_RPC_PATH = /1e8cfBC369ADDc93d135
+
+# Connect to the Ethereum node via Web3 / RPC
+web3 = Web3::Eth::Rpc.new host: ETHEREUM_NODE_HOST,
+                          port: ETHEREUM_NODE_PORT,
+                          connect_options: {
+                            open_timeout: ETHEREUM_NODE_OPEN_TIMEOUT,
+                            read_timeout: ETHEREUM_NODE_READ_TIMEOUT,
+                            use_ssl:  ETHEREUM_NODE_USE_SSL,
+                            rpc_path: ETHEREUM_NODE_RPC_PATH
+                          }
+
+# Get the latest block’s number
+latest_block = web3.eth.blockNumber
