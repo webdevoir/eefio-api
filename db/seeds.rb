d6db9bbe077ac795b9a88a8ca6c52e2fa5b5f112
Shane Becker <veganstraightedge@gmail.com>
2018-05-25 16:52:48 -0700

    WIP: improve seed script to find and fill in missing blocks

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,18 +28,22 @@ web3 = Web3::Eth::Rpc.new host: ETHEREUM_NODE_HOST,
 latest_block_number = web3.eth.blockNumber
 
 # Get the latest RawBlock from the database
-latest_raw_block        = RawBlock.order(block_number: :desc).limit(1).first
-latest_raw_block_number = latest_raw_block&.block_number || 0
+raw_blocks_count = RawBlock.count || 0
+
+puts "latest_block_number: #{latest_block_number}"
+puts "raw_blocks_count:    #{raw_blocks_count}"
 
 # Exit if the database is synced up with the blockchain
-if latest_raw_block_number >= latest_block_number
+# The +1 is because the first block is 0.
+# Eg, If latest_block_number is 2. The database will have be RawBlocks: 0, 1, 2.
+if raw_blocks_count == (latest_block_number + 1)
   puts "RawBlocks synced with Ethereum blockchain!"
   exit 0
 end
 
 # Setup the number of the next block to import
 # Fallback to 0 if there are no RawBlocks yet
-next_block_number = latest_raw_block.blank? ? 0 : (latest_raw_block_number + 1)
+next_block_number = raw_blocks_count
 
 
 # Use our own thread pool
@@ -54,23 +58,32 @@ pool = Concurrent::ThreadPoolExecutor.new(
 
 
 # Work through the blockchain in groups of blocks at a time
-(next_block_number..latest_block_number).each_slice(HTTP_THREAD_COUNT) do |slice|
+(next_block_number..latest_block_number).each_slice(HTTP_THREAD_COUNT) do |block_numbers|
   # Create all of the promisess of work to do: get a block, save it to the database
   promises = []
 
-  slice.each do |block_number|
+  block_numbers.each do |block_number|
+    puts block_number
+
     promise = Concurrent::Promise.new(executor: pool) do
-      # Get the next block from the Ethereum node
-      block = web3.eth.getBlockByNumber block_number
+      raw_block = RawBlock.find_by block_number: block_number
 
-      ActiveRecord::Base.connection_pool.with_connection do
-        # Save the block to the raw_blocks table in the database
-        raw_block = RawBlock.create! block_number: block.block_number, content: block.raw_data.to_json
-        puts "Saved block: #{raw_block.block_number}"
+      if raw_block.present?
+        puts "RawBlock already exists: #{block_number}"
+      else
+        puts "==> Fetching block from chain: #{block_number}"
+        block = web3.eth.getBlockByNumber block_number
+
+        ActiveRecord::Base.connection_pool.with_connection do
+          # Save the block to the raw_blocks table in the database
+          raw_block = RawBlock.create block_number: block.block_number, content: block.raw_data.to_json
+          puts "    Saved block: #{raw_block.block_number}"
+        end
+
       end
-    end
 
-    promises << promise.execute
+      promises << promise.execute
+    end
   end
 
   # Do the work in all of the promises: get a block, save it to the database
@@ -80,6 +93,36 @@ pool = Concurrent::ThreadPoolExecutor.new(
   puts "RawBlocks now in the database: #{RawBlock.count}"
   puts
 end
+
+
+puts
+puts "Latest block on blockchain (at start of sync): #{latest_block_number}"
+puts "RawBlocks now in the database:                 #{RawBlock.count}"
+puts
+
+exit
+
+puts "Checking for missing blocks and re-fetching them"
+latest_raw_block = RawBlock.order(block_number: :desc).limit(1).first
+if latest_raw_block.block_number > RawBlock.count
+  (0..latest_block_number).each do |block_number|
+    raw_block = RawBlock.find_by block_number: block_number
+
+    if raw_block.present?
+      puts "RawBlock already exists: #{block_number}"
+    else
+      puts "==> Fetching block from chain: #{block_number}"
+      block = web3.eth.getBlockByNumber block_number
+
+      ActiveRecord::Base.connection_pool.with_connection do
+        # Save the block to the raw_blocks table in the database
+        raw_block = RawBlock.create block_number: block.block_number, content: block.raw_data.to_json
+        puts "    Saved block: #{raw_block.block_number}"
+      end
+    end
+  end
+end
+
 
 puts
 puts "Latest block on blockchain (at start of sync): #{latest_block_number}"
