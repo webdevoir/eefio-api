a9cf980604c03dec688bfa7bf0a014f12e295dec
Shane Becker <veganstraightedge@gmail.com>
2018-05-26 19:21:12 -0700

    Add uniqueness validation to Block for block_number, block_number_in_hex, and address

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,2 +1,5 @@
 class Block < ApplicationRecord
+  validates :block_number,        presence: true, uniqueness: true
+  validates :block_number_in_hex, presence: true, uniqueness: true
+  validates :address,             presence: true, uniqueness: true
 end
