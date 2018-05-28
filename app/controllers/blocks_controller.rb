325680e3902084d90120379dd012d83a89027fb0
Shane Becker <veganstraightedge@gmail.com>
2018-05-27 19:50:35 -0700

    Add *very simple* API route for /blocks/:address and /blocks/:block_number

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,6 +1,18 @@
 class BlocksController < ApplicationController
   def show_latest
     @block = Block.order(block_number: :desc).limit(1).first
+
+    render json: @block
+  end
+
+  def show
+    @block =
+      if params[:id].strip.downcase[0..1] == '0x'
+        Block.find_by(address: params[:id])
+      else
+        Block.find_by(block_number: params[:id])
+      end
+
     render json: @block
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get 'blocks/latest', to: 'blocks#show_latest', as: :latest_block
+
+  # /blocks/123    find by block number
+  # /blocks/0xACAB find by block address
+  get 'blocks/:id', to: 'blocks#show', as: :block
 end
