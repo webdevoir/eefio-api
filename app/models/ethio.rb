1fa16b7ccf38939b2a99628ea923a0230f2df565
Shane Becker <veganstraightedge@gmail.com>
2018-05-29 17:43:03 -0700

    Update License URL

--- app/models/ethio.rb
+++ app/models/ethio.rb
@@ -1,5 +1,5 @@
 class Ethio
-  LICENSE           = 'CC0 / https://creativecommons.org/choose/zero'.freeze
+  LICENSE           = 'CC0 / http://creativecommons.org/publicdomain/zero/1.0'.freeze
   API_URL           = (ENV['ETHIO_API_URL']           || 'https://api.ethio.app').freeze
   DOCUMENTATION_URL = (ENV['ETHIO_DOCUMENTATION_URL'] || 'https://ethio.app').freeze
   CONTRIBUTORS      = [
