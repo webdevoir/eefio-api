5eb834ef71e2612e4087dc64deda8d069deacca8
Shane Becker <veganstraightedge@gmail.com>
2018-07-19 21:14:12 -0700

    Extract Block after a RawBlock is created

--- app/models/raw_block.rb
+++ app/models/raw_block.rb
@@ -1,9 +1,16 @@
 class RawBlock < ApplicationRecord
   validates :block_number, presence: true, uniqueness: true
+  after_create :extract_block
 
   class << self
     def last_in_sync_block_number
       Setting.find_by(name: 'raw_blocks_previous_synced_at_block_number').content.to_i
     end
+  end
+
+  def extract_block
+    puts "==> Extracting Block from RawBlock: #{self.id}…"
+    BlockExtractorService.extract_block_from raw_block: self
+    puts "==> Extracting Block from RawBlock: #{self.id}… done."
   end
 end
