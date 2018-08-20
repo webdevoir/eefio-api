7d6d209404eeb52301a5b3579be65744b0f84754
Shane Becker <veganstraightedge@gmail.com>
2018-08-19 22:04:31 -0700

    Disable uniqueness validations on Block
    To hopefully speed up database IO time when this table it big

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,7 +1,7 @@
 class Block < ApplicationRecord
-  validates :block_number,        presence: true, uniqueness: true
-  validates :block_number_in_hex, presence: true, uniqueness: true
-  validates :address,             presence: true, uniqueness: true
+  validates :block_number,        presence: true #, uniqueness: true
+  validates :block_number_in_hex, presence: true #, uniqueness: true
+  validates :address,             presence: true #, uniqueness: true
 
   URL_NAMESPACE = 'blocks'.freeze
 
