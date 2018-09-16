1af645172ca06525f0dc42248d894294fec599f8
Shane Becker <veganstraightedge@gmail.com>
2018-09-16 01:21:46 -0700

    Remove uniqueness validation on RawBlock

--- app/models/raw_block.rb
+++ app/models/raw_block.rb
@@ -1,5 +1,5 @@
 class RawBlock < ApplicationRecord
-  validates :block_number, presence: true
+  validates :block_number, presence: true, uniqueness: true
 
   class << self
     def last_in_sync_block_number
