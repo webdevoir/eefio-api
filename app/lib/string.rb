c8c1338560470c7a779e10a9ec6ca400ba8e695b
Shane Becker <veganstraightedge@gmail.com>
2018-09-25 14:20:46 -0700

    Move #to_hex and #from_hex to an initializer

--- app/lib/string.rb
+++
@@ -1,6 +0,0 @@
-class String
-  # Usage: '0xACAB'.from_hex #=> 44203
-  def from_hex
-    to_i 16
-  end
-end
--- config/initializers/monkey_patches.rb
+++ config/initializers/monkey_patches.rb
@@ -1,3 +1,10 @@
+class String
+  # Usage: '0xACAB'.from_hex #=> 44203
+  def from_hex
+    to_i 16
+  end
+end
+
 class Integer
   # Usage: 8675309.to_hex #=> 0x845fed
   def to_hex
