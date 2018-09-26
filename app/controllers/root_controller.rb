9ffb8189bbd30a067157de0940d2a240d1ad8f4f
Shane Becker <veganstraightedge@gmail.com>
2018-09-25 18:55:11 -0700

    Add root route docs for blocks#raw

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,25 +1,29 @@
 class RootController < ApplicationController
   def index
-    @links = { self: Eefio::API_URL }
+    @links = {
+      self:          Eefio::API_URL,
+      blocks_latest: api_url_for(path: '/blocks/latest'),
+      blocks_first:  api_url_for(path: '/blocks/0')
+    }
 
     @endpoints = I18n.t('api.endpoints').map do |endpoint|
       {
-        name:         endpoint[:name],
-        http_verb:    'GET',
-        url:          api_url_for(name: endpoint[:name]),
-        documenation: documenation_url_for(name: endpoint[:name]),
-        description:  endpoint[:description]
+        name:          endpoint[:name],
+        http_verb:     'GET',
+        url:           api_url_for(path: endpoint[:path]),
+        documentation: documentation_url_for(name: endpoint[:name]),
+        description:   endpoint[:description]
       }
     end
   end
 
-  def documenation_url_for name:
+  private
+
+  def documentation_url_for name:
     [Eefio::DOCUMENTATION_URL, name.split('#')].join('/')
   end
 
-  def api_url_for name:
-    [Eefio::API_URL, name.split('#')].join('/')
-                                     .gsub('/one', '/:identifier')
-                                     .gsub('/root', '')
+  def api_url_for path:
+    [Eefio::API_URL, path].join('/')
   end
 end
